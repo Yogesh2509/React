0183c0ca404549b10b3b4266efb6b136569930d1
Yogesh Mehra <yogeshmehra2509@gmail.com>
2024-03-11 12:42:09 +0530

    React

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,16 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import AddIcon from '@mui/icons-material/Add';
 import Button from '@mui/material/Button';
 import "./App.css"
+import RecordsProject from './RecordsProject';
 const App = () => {
+  const [name, setName]=useState("")
+  const  [email,setEmail] = useState("");
+  const [data, setData] = useState([]);
+  function addData(){
+    setData({
+      name:name,email:email
+    })
+  }
   return (
-    <Stack id='container'spacing={2} direction="row">
-      <TextField id="outlined-basic" label="Name" variant="outlined" />
-      <TextField id="outlined-basic" label="Email" variant="outlined" />
-      <Button variant="contained">{<AddIcon/>}</Button>
+    <div id='app'>
+      <RecordsProject/>
+      <div id='heading'>
+      <Stack id='container' spacing={20} direction="row">
+      <TextField value={name}
+      onChange={(event)=>setName(event.target.value)}
+      id="outlined-basic" 
+      label="Name" 
+      variant="outlined" />
+      <TextField value={email}
+      onChange={(event)=>setEmail(event.target.value)}
+      id="outlined-basic" 
+      label="Email" 
+      variant="outlined" />
+      <Button onClick={addData} variant="contained">{<AddIcon/>}</Button>
       </Stack> 
+      </div>
+      <div className='data'>
+        <div className='data_value'>
+      <h1>Name</h1>
+      <h1>Email</h1>
+      <h1>Remove</h1>
+      </div>
+      </div>
+      </div>
   )
 }
 
---
+++ vite-project/src/RecordsProject.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RecordsProject = () => {
+  return (
+    <div id='records'>
+        <h1>Employee Records</h1>
+    </div>
+  )
+}
+
+export default RecordsProject
