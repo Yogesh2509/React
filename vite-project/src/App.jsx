a71611617fea329cb366c4d678fff7e25b9960bb
Yogesh Mehra <yogeshmehra2509@gmail.com>
2024-03-14 19:39:17 +0530

    React

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,62 +1,12 @@
-import React, { useState } from 'react'
-import TextField from '@mui/material/TextField';
-import Stack from '@mui/material/Stack';
-import AddIcon from '@mui/icons-material/Add';
-import Button from '@mui/material/Button';
-import "./App.css"
-import RecordsProject from './RecordsProject';
-const App = () => {
-  const [name, setName]=useState("")
-  const  [email,setEmail] = useState("");
-  const [data, setData] = useState([]);
-  function addData(){
-    setData([...data,{name:name,email:email}]);
-    setName("");
-    setEmail("");
-  }
-  function removeItem(index){
-    let arr=data;
-    arr.splice(index,1)
-    setData([...arr])
-  }
-  return (
-    <div id='app'>
-      <RecordsProject/>
-      <div id='heading'>
-      <Stack id='container' spacing={20} direction="row">
-      <TextField value={name}
-      onChange={(event)=>setName(event.target.value)}
-      id="outlined-basic" 
-      label="Name" 
-      variant="outlined" />
-      <TextField value={email}
-      onChange={(event)=>setEmail(event.target.value)}
-      id="outlined-basic" 
-      label="Email" 
-      variant="outlined" />
-      <Button onClick={addData} variant="contained">{<AddIcon/>}</Button>
-      </Stack> 
-      </div>
-      <div className='data'>
-        <div className='data_value'>
-      <h1>Name</h1>
-      <h1>Email</h1>
-      <h1>Remove</h1>
-      </div>
-      {
-        data.map((element,index) =>{
-          return(
-            <div className='data_value'>
-              <h4>{element.name}</h4>
-              <h4>{element.email}</h4>
-              <Button onClick={()=>removeItem(index)}>Remove</Button>
-            </div>
-          )
-        })
-}
-      </div>
-      </div>
-  )
-}
+import React, { useEffect, useState } from 'react'
 
+const App = () => {
+  const [state,setuser]=useState("")
+  useEffect(()=>{
+    alert("Use effect hook is called")
+})
+    alert("Outside Function")
+return (
+  <button onClick={()=>{setState(state+1)}}>Click me {state}</button>
+)
 export default App
