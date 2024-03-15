7bf5695f36c83c3707023a55f37d61db655bc6d1
Yogesh Mehra <yogeshmehra2509@gmail.com>
2024-03-15 12:19:30 +0530

    React

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,27 @@
 import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const App = () => {
-  const [state,setuser]=useState("")
+  const [employee,setEmployee]=useState([])
   useEffect(()=>{
-    alert("Use effect hook is called")
-})
-    alert("Outside Function")
+    axios.get(`https://hub.dummyapis.com/employee?noofRecords=&idStarts=1001`).
+    then(employee=>setEmployee(employee.data)).catch(err=>console.log(err)) 
+    },[])     
+   
 return (
-  <button onClick={()=>{setState(state+1)}}>Click me {state}</button>
+  <div>
+  {
+    employee.map((e)=>{
+      return(
+        <div>
+        <h1>{e.firstName}</h1>
+        <h2>{e.age}</h2>
+        <h3>{e.dob}</h3>
+        </div>
+      )
+    })
+  }
+  </div>
 )
+}
 export default App
