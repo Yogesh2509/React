7c17200e79efd79bd01fb6016e2eeefd5e418722
Yogesh Mehra <yogeshmehra2509@gmail.com>
2024-03-16 21:59:16 +0530

    React

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,23 +1,41 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import Employee from './Employee';
 
 const App = () => {
+  const [state,setState]=useState(0);
   const [employee,setEmployee]=useState([])
   useEffect(()=>{
-    axios.get(`https://hub.dummyapis.com/employee?noofRecords=&idStarts=1001`).
-    then(employee=>setEmployee(employee.data)).catch(err=>console.log(err)) 
-    },[])     
+    async function getData(){
+      const data = await fetch(`https://hub.dummyapis.com/employee?noofRecords=${state}&idStarts=1001`)
+      const dataJson=await data.json();
+      setEmployee(dataJson)
+    }
+   // axios.get(`https://hub.dummyapis.com/employee?noofRecords=&idStarts=1001`).
+  //then(employee=>setEmployee(employee.data)).catch(err=>console.log(err)) 
+    getData();
+  },[state])     
    
 return (
   <div>
+    <button onClick={()=>{setState(state+5)}}>Load More{state}</button>
+    <table>
+      <tr>
+        <th>First Name</th>
+        <th>Age</th>
+        <th>Email</th>
+      </tr>
+    </table>
   {
     employee.map((e)=>{
       return(
-        <div>
-        <h1>{e.firstName}</h1>
-        <h2>{e.age}</h2>
-        <h3>{e.dob}</h3>
-        </div>
+     <div>
+      <Employee
+      Name={e.firstName}
+      age={e.age}
+      email={e.email}
+      />
+     </div>
       )
     })
   }
